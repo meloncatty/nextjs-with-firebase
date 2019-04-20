d29c4373dbc8afc30433b735cb8a47a18017ed0f
Krista Handel <k.handel66@gmail.com>
2019-04-20 14:53:27 -0600

    update next config

--- next.config.js
+++ next.config.js
@@ -47,25 +47,5 @@ module.exports = withAssetRelocator({
 
     return config
   },
-  exportPathMap: function () {
-    return {
-        "/": { page: "/" },
-        '/product/1': { page: '/product', query: { id: "1" } },
-        '/product/2': { page: '/product', query: { id: "2" } },
-        '/product/3': { page: '/product', query: { id: "3" } },
-        '/product/4': { page: '/product', query: { id: "4" } },
-        '/product/5': { page: '/product', query: { id: "5" } },
-        '/product/6': { page: '/product', query: { id: "6" } },
-        '/product/7': { page: '/product', query: { id: "7" } },
-        '/product/8': { page: '/product', query: { id: "8" } },
-        '/product/9': { page: '/product', query: { id: "9" } },
-        '/product/10': { page: '/product', query: { id: "10" } },
-        '/product/11': { page: '/product', query: { id: "11" } },
-        '/product/12': { page: '/product', query: { id: "12" } },
-        '/product/13': { page: '/product', query: { id: "13" } },
-        '/product/14': { page: '/product', query: { id: "14" } },
-        '/product/15': { page: '/product', query: { id: "15" } }
-    }
-  },
   target: 'serverless'
 })
