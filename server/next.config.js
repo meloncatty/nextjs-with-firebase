cd8bcc96e0e1c766307d5c286e8acc9cd969affe
Krista Handel <k.handel66@gmail.com>
2019-04-20 12:55:18 -0600

    add webpack asset relocator loader

--- server/next.config.js
+++ server/next.config.js
@@ -1,8 +1,42 @@
 const webpack = require('webpack')
-
 require('dotenv').config()
 
-module.exports = {
+const withAssetRelocator = (nextConfig = {}) => {
+  return Object.assign({}, nextConfig, {
+    webpack(config, options) {
+      const { isServer } = options
+      
+      if (isServer) {
+        config.node = Object.assign({}, config.node, {
+          __dirname: false,
+          __filename: false
+        })
+
+        config.module.rules.unshift({
+          test: /\.(m?js|node)$/,
+          parser: { amd: false },
+          use: {
+            loader: '@zeit/webpack-asset-relocator-loader',
+            options: {
+              outputAssetBase: 'assets',
+              existingAssetNames: [],
+              wrapperCompatability: true,
+              escapeNonAnalyzableRequires: true
+            }
+          }
+        })
+      }
+
+      if (typeof nextConfig.webpack === 'function') {
+        return nextConfig.webpack(config, options)
+      }
+
+      return config
+    }
+  })
+}
+
+module.exports = withAssetRelocator({
   webpack: config => {
     const env = Object.keys(process.env).reduce((acc, curr) => {
       acc[`process.env.${curr}`] = JSON.stringify(process.env[curr])
@@ -34,4 +68,4 @@ module.exports = {
     }
   },
   target: 'serverless'
-}
+})
